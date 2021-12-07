d07f5e09e77bc3d12696488f4196fe2b973b230e
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-12-07 09:51:18 +0100

    Day 07

---
+++ 07/07_p2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+
+with open(sys.argv[1], "r") as f:
+    data = [int(x.strip()) for x in f.readlines()[0].split(",")]
+
+max_hrz = max(data)
+min_hrz = min(data)
+
+less_fuel = None
+best_hrz_pos = None
+
+for i in range(min_hrz, max_hrz + 1):
+    fuel = 0
+    for h in data:
+        fuel_cost = 1
+        while h != i:
+            if h > i:
+                h -= 1
+                fuel += fuel_cost
+            elif h < i:
+                h += 1
+                fuel += fuel_cost
+            fuel_cost += 1
+            if less_fuel is not None and fuel > less_fuel:
+                break
+        if less_fuel is not None and fuel > less_fuel:
+            break
+    if less_fuel is None or fuel < less_fuel:
+        less_fuel = fuel
+        best_hrz_pos = i
+
+print(f"Pos: {best_hrz_pos}, Fuel: {less_fuel}")
